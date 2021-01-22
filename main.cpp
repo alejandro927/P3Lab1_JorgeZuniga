ba14676f49456fae1de6094f39be6e742c0da530
JorgeZuniga <alejandrojorge927@gmail.com>
2021-01-22 14:39:36 -0600

    hicimos el primer ejercicio :v

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
@@ -12,25 +13,40 @@ int main(int argc, char** argv) {
 		cout<<"1)Ejercicio1\n";
 		cout<<"2)Ejercicio2\n";
 		cout<<"Elija su opcion:\n";
+		cout<<"3)salida salida\n";
 		cin>>opcion;
-		cout<<"Su opcion fue:"<<opcion<<"\n";
+		
 		switch(opcion){
+			
 		case 1:{
-			cout<<"\n1)Ejercicio1\n";
+			int n=0;
+			cout<<"\nIngrese el numero para la sumatoria: ";
+			cin>> n;
 			
-			
-			
+			while(n<0){
+				cout<<"No se admiten numeros negativos ingrese nuevamente:";
+				cin>>n;
+			}
+			int acum=0;
+			for(int i=0;i<=n;i++){
+				
+				acum += i*pow(i+1,2);
+			}
+			cout<<"La respuesta es= "<<acum;
+			cout<<"\n";
 		}
 		break;
-		case  2:{
+		case 2:{
 			cout<<"\n2)Ejercicio2\n";
 			
 		}
 		break;
-		default:
+		case 3:
 			cout<<"\nAdios!!";
+			cin>>respuesta;
 			respuesta==2;
-			
+		
+		break;
 		}
 	}
 	return 0;
